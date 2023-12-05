2bbfe093e97ba356b84bb4cd771466c4e0a78716
Angela Matiiva <angelamatiiva@gmail.com>
2023-12-05 19:50:54 +0200

    first commit

---
+++ src/main/java/com/code/angel/countrycitylist/CountryCityListApplication.java
@@ -0,0 +1,13 @@
+package com.code.angel.countrycitylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCityListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryCityListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/code/angel/countrycitylist/CountryCityListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.angel.countrycitylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryCityListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
